f1240b172b6929f429a9f3f8679f5d80addaf9d5
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-02-04 00:01:40 +0800

    Woo Hoo! \o/

--- README.md
+++ README.md
@@ -1,7 +1,72 @@
 Click Stream
 ===========
 
+Learning about [Streams](http://elixir-lang.org/docs/master/elixir/Stream.html) in Elixir. 
+
+This project creates a wXwidget window, and by moving the mouse, emits mouse coordinates.
+
+## Playing Around
+
+Start the project in `iex`:
+
 ```
 % iex -S mix
-iex> ClickStream.start
+```
+
+In this example, two streams are created: 
+
+* `ClickStream.create_stream_x` creates a stream that produces values that represent the x coordinates of the mouse.
+
+* `ClickStream.create_stream_y` creates a steam that produces vlaues that represent the y coordinates of the mouse.
+
+### Streams are lazy:
+
+```elixir
+iex> stream |> Stream.filter(fn x -> x < 100 end) |> Enum.take 10
+x: 376
+x: 376
+x: 376
+x: 13
+x: 13
+x: 15
+x: 16
+x: 17
+x: 18
+x: 20
+x: 21
+x: 22
+x: 22
+[13, 13, 15, 16, 17, 18, 20, 21, 22, 22]
+```
+
+### Streams are composable:
+
+We can lazily zip collections together:
+
+```elixir
+iex> Stream.zip(ClickStream.create_stream_x, ClickStream.create_stream_y)
+#Function<6.29647706/2 in Stream.zip/2>
+iex> Stream.zip(ClickStream.create_stream_x, ClickStream.create_stream_y) |> Enum.take 10
+x: 380
+y: 144
+x: 359
+y: 147
+x: 340
+y: 150
+x: 329
+y: 153
+x: 316
+y: 157
+x: 307
+y: 160
+x: 299
+y: 160
+x: 298
+y: 156
+x: 296
+y: 154
+x: 282
+y: 148
+[{380, 144}, {359, 147}, {340, 150}, {329, 153}, {316, 157},
+ {307, 160}, {299, 160}, {298, 156}, {296, 154}, {282, 148}]
 ```
--- lib/click_stream.ex
+++ lib/click_stream.ex
@@ -4,26 +4,53 @@ defmodule ClickStream do
 
   @title 'Click Stream'
 
-  def start do
+  def create_stream_x do
+    Stream.resource(fn -> create_frame end,
+                    fn(frame) -> loop_x(frame) end,
+                    fn(frame) -> destroy_frame(frame) end)
+  end 
+
+  def create_stream_y do
+    Stream.resource(fn -> create_frame end,
+                    fn(frame) -> loop_y(frame) end,
+                    fn(frame) -> destroy_frame(frame) end)
+  end 
+
+  def create_frame do
     wx    = :wx.new
     frame = :wxFrame.new(wx, -1, @title)
     :wxWindow.connect(frame, :close_window)
     :wxWindow.connect(frame, :motion)
     :wxFrame.show(frame)
-    loop(frame)
-    :wxFrame.destroy(frame)
+    frame
+  end
+  
+  def loop_x(frame) do
+    receive do
+      {:wx, _, _, _, {:wxMouse, :motion, x, _y, _, _, _, _, _, _, _, _, _, _}} ->
+        IO.puts "x: #{x}"
+        {[x], frame}
+      {:wx, _, {:wx_ref, _, :wxFrame, []}, [], {:wxClose, :close_window}} ->
+        {:halt, frame}
+      _ ->
+        {:halt, frame}
+    end
   end
 
-  def loop(frame) do
+  def loop_y(frame) do
     receive do
-      {:wx, _, _, _, {:wxMouse, :motion, x, y, _, _, _, _, _, _, _, _, _, _}} ->
-        IO.puts "x: #{x}, y: #{y}"
-        loop(frame) 
+      {:wx, _, _, _, {:wxMouse, :motion, _x, y, _, _, _, _, _, _, _, _, _, _}} ->
+        IO.puts "y: #{y}"
+        {[y], frame}
       {:wx, _, {:wx_ref, _, :wxFrame, []}, [], {:wxClose, :close_window}} ->
-        IO.puts "closing..."
+        {:halt, frame}
       _ ->
-        loop(frame) 
+        {:halt, frame}
     end
+  end
+
+  def destroy_frame(frame) do
+    :wxFrame.destroy(frame)
   end
 
 end
